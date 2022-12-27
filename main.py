2eb8e61ae92142e610b0941d99f4c7d3694fbb06
FranciscoSerafini <franciscoserafinigiorgi@gmail.com>
2022-12-27 16:17:03 -0300

    nueva estructura de elemenos de programa: Item

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 #usamos esta ventana main.py como prueba para ver si funcionaba uvicorn y fastapi
 from typing import Union
-
 from fastapi import FastAPI
 
+
+from models.item_model import Item
+
+
 app = FastAPI()
+
 
 #tipos de parametros de una ruta
 @app.get("/")
 def read_root():
     return {"Hola": "Mundooo!!!!!!"}
-
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
@@ -18,5 +20,12 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.get('/calculadora')
 def calcular(operando1:float, operando2:float):
     return {'suma':operando1 + operando2}
+
+@app.put('/items/{item_id}')
+def update_item(item_id:int, item: Item):
+    return{'item_name': item.name, 'item_id': item_id, 'item_price': item.price}
+
+
+
 
 
