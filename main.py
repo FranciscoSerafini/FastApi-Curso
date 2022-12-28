29adb7398fcb2546bf1f1e59c51876e4dcba2cbc
FranciscoSerafini <franciscoserafinigiorgi@gmail.com>
2022-12-28 10:12:22 -0300

    resolucion de error de atributos

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def calcular(operando1:float, operando2:float):
 
 @app.put('/items/{item_id}')
 def update_item(item_id:int, item: Item):
-    return{'item_name': item.name, 'item_id': item_id, 'item_price': item.price}
+    return{'item_name': item.name, 'item_id': item_id, 'item_price':item.price}
 
 
 
