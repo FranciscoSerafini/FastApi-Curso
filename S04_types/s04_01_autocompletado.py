d9f448ad8462b80c95953507a05bc732657a81b9
FranciscoSerafini <franciscoserafinigiorgi@gmail.com>
2022-12-28 10:14:50 -0300

    correcion de error de atributo

--- S04_types/s04_01_autocompletado.py
+++ S04_types/s04_01_autocompletado.py
@@ -1,3 +1,6 @@
 def get_full_name(first_name, last_name):
     full_name = first_name.title() + '' + last_name.title()
     return full_name
+
+print(get_full_name('Fran', 'Serafini'))
+#print(get_full_name('Fran', '0')) error de atributo, ya que si le pasariamos un entero el atributo title no puede recibir enteros
