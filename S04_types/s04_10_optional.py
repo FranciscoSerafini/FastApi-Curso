7c20b124fbcdf1674362e031d02927ac51e052ca
FranciscoSerafini <franciscoserafinigiorgi@gmail.com>
2022-12-29 23:28:54 -0300

    usar el operador | para parametros opcionales

--- S04_types/s04_10_optional.py
+++ S04_types/s04_10_optional.py
@@ -1,7 +1,7 @@
 from typing import Optional
 #importamos Optional
 def say_hi ( name: Optional [str] = None ):
-   if name is not None :
-        print('Hola {name} ')
-    else:
+   if name is not None:
+        print('Hola {name}')
+   else:
         print('Hola mundo!')
