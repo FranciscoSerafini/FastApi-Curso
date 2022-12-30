3c3b68fb3413126e88f4009f62fee072b2fef906
FranciscoSerafini <franciscoserafinigiorgi@gmail.com>
2022-12-30 00:29:38 -0300

    clase para representar un producto

--- practicas_activas/api_productos.py
+++ practicas_activas/api_productos.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
+#creamos un modelo
+class producto(BaseModel):
+    id:Optional[str]
+    nombre:str
+    precio_compra:float
+    precio_venta:float
+    proveedor:str
+
+    
 app = FastAPI() 
+#creamos una lista de prodcutos
+productos = []
+
 @app.get('/') #ruta de bienvenida
 def index():
     return{'mensaje':'Bienvenidos a la API de prodcutos'}
